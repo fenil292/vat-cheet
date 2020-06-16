dfccc213000533dde0315e956c711dfcc718cb74
fenil292 <65783083+fenil292@users.noreply.github.com>
2020-06-16 21:52:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+var express=require('express');
+var socket=require('socket.io');
+var http = require('http');
+var app=express();
+const port = process.env.PORT || 8080;
+var server = http.createServer(app);
+/*var serever=app.listen(8000,function(){
+	console.log("hello world");
+});*/
+var io = socket.listen( server );
+
+io.sockets.on( 'connection', function( client ) {
+	console.log( "New client !" );
+	
+	client.on( 'message', function( data ) {
+		console.log( 'Message received ' + data.name + ":" + data.message + ":" + data.time);
+		//client.broadcast.emit( 'message', { name: data.name, message: data.message } );
+		io.sockets.emit( 'message', { name: data.name, message: data.message,time: data.time } );
+	});
+});
+
+server.listen( port );
+//app.use(express.static('public'));
