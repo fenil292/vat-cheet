59aac21d7c18ebcef7929e4a599f0faa09df5849
fenil292 <65783083+fenil292@users.noreply.github.com>
2020-06-16 15:51:19 +0530

    Update nodec.js

--- js/nodec.js
+++ js/nodec.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://vat-cheet.herokuapp.com:8080');
+var socket = io.connect('https://vat-cheet.herokuapp.com');
 $( "#send" ).click( function() {
 	var m = $( "#msgbox" ).val();
 	var name= document.getElementById('name');
