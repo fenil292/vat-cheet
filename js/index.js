a1e4fe9c2a71a1ca8cb7bc656386425a98bfa3cd
fenil292 <65783083+fenil292@users.noreply.github.com>
2020-06-17 22:58:01 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var express=require('express');
 var socket=require('socket.io');
 var http = require('http');
 var app=express();
-//const path = require('path');
+const path = require('path');
 const port = process.env.PORT || 8080;
 var server = http.createServer(app);
 /*var server = http.createServer(function(req,res){
@@ -11,11 +11,12 @@ var server = http.createServer(app);
 /*var serever=app.listen(port,function(){
 	console.log("hello world");
 	console.log(path.join(__dirname + '/../../index.php'));
-});
+});*/
 app.get('/',function(req,res){
 	//res.redirect("../index.php");
-	res.redirect(path.join(__dirname + '/../../index.php'));
-});*/
+	//res.redirect(path.join(__dirname + '/../../index.php'));
+	res.sendFile(path.join(__dirname+'/../index.php'));
+});
 
  var io = socket.listen( server );
 
