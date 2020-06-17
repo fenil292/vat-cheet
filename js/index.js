c776f4c6a2c763d0c66bf1840f34a9d3dd187f83
fenil292 <65783083+fenil292@users.noreply.github.com>
2020-06-18 00:02:16 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index');
+    res.redirect('index');
 });
 
  var io = socket.listen( server );
