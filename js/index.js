6ccf032853c1d4c2dc97dcc35f839c26dab56bd3
fenil292 <65783083+fenil292@users.noreply.github.com>
2020-06-17 23:58:03 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,11 +13,22 @@ var server = http.createServer(app);
 	console.log("hello world");
 	console.log(path.join(__dirname + '/../index.php'));
 });*/
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
 	//res.redirect("/../index.php");
 	//res.redirect(path.join(__dirname + '/../../index.php'));
 	//s.sendFile(path.join(__dirname+'/../index.php'));
 	res.redirect(path.join(__dirname+'/../index.php'));
+});*/
+app.set('view engine', 'php');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
  var io = socket.listen( server );
