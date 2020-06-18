0053c79ab1ccafea90c942b20c8fd739f3d6803f
fenil292 <65783083+fenil292@users.noreply.github.com>
2020-06-18 12:21:59 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,12 +15,13 @@ var server = http.createServer(app);
 });*/
 app.get('/',function(req,res){
           //res.send(path.join(__dirname));
-	//res.redirect("/../index.php");
+	return res.redirect("/../index.php");
         //res.sendFile(path.join(__dirname+'/../index.php'));
 	//res.redirect(path.join(__dirname + '/../index.php'));
 	//s.sendFile(path.join(__dirname+'/../index.php'));
 	//res.redirect(path.join(__dirname+'/../index.php'));
-        res.sendFile('/index.php', { root: __dirname })
+        //res.sendFile('/index.php', { root: __dirname })
+
 });
  var io = socket.listen( server );
 
