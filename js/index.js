bbfe453f7f3b87c3629c7a72905c308807caaa8e
fenil292 <65783083+fenil292@users.noreply.github.com>
2020-07-16 18:56:17 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ var server = http.createServer(app);
 	console.log("hello world");
 	console.log(path.join(__dirname + '/../index.php'));
 });*/
-app.get('/',function(req,res){
+/*app.get('/',function(req,res){
           //res.send(path.join(__dirname));
 	//res.send('<script>window.location.href="/../index.php";</script>');
 	//res.sendFile("/../index.php");
@@ -23,7 +23,7 @@ app.get('/',function(req,res){
 	//res.redirect(path.join(__dirname+'/../index.php'));
         //res.sendFile('/index.php', { root: __dirname })
 
-});
+});*/
  var io = socket.listen( server );
 
  io.sockets.on( 'connection', function( client ) {
@@ -38,5 +38,5 @@ app.get('/',function(req,res){
 /*app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });*/
-app.listen(port);
+//app.listen(port);
 //app.use(express.static('public'));
